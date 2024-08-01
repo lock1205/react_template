3cefa6f4d70b66614557fa0632ddcc32fceecdaf
lock1205 <mindheal205@gmail.com>
2024-08-01 13:10:11 +0900

    initial setting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <div className="kr">첫 페이지 구성</div>
+      <div className="en">Create First Page</div>
     </div>
   );
-}
+};
 
 export default App;
